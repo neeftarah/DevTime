0a7a557b9b10af2876fae4f473f960a760fdf65c
Jérémy MOREAU <morea_j35@auditeur.cnam.fr>
2025-05-06 19:24:09 +0200

    Initialize DDD structure, database and AMQP

---
+++ src/Client/Controller/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Client\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{
+    #[Route('/hello', name: 'hello')]
+    public function __invoke(): Response
+    {
+        return new Response('Hello Symfony DDD!');
+    }
+}
