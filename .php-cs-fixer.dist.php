ce65f5b7ebc0108c0c5f58e99326e1f92b58b473
Jérémy MOREAU <morea_j35@auditeur.cnam.fr>
2025-07-24 18:59:05 +0200

    Add GrumPHP

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,12 +2,14 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->exclude('var')
-;
+    ->exclude([
+        'var',
+        'vendor',
+        'node_modules',
+    ]);
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
     ])
-    ->setFinder($finder)
-;
+    ->setFinder($finder);
